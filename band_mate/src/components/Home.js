7c8839dd7370065a4805bf34ba9d01678a9e068d
Cesar Cisneros <cesarcisneros9@gmail.com>
2021-07-22 23:34:14 -0500

    made body background color main color

--- band_mate/src/components/Home.js
+++ band_mate/src/components/Home.js
@@ -4,7 +4,7 @@ import Box from '@material-ui/core/Box'
 const Home = () => {
   return (
     <div>
-      <Box bgcolor="#212121" color="#9e9e9e" height="100vh" p={2}>
+      <Box bgcolor="#212121" color="#9e9e9e" p={2}>
         <h1 style={{textAlign: "center"}}>Welcome to my App. It is currently under construction!</h1>
         <h2 style={{textAlign: "center"}}>View all Posts is the only working link at the moment.</h2>
       </Box>
